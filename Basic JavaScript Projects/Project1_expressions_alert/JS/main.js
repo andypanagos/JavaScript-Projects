ec5a6f51ce812aa4293818d08e049e72668eb513
andypanagos <andypanagos2@gmail.com>
2022-06-29 13:32:09 -0400

    Java

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,26 @@
+window.alert("Hello,World");
+
+document.write('Hello, world');
+
+var A = "This is a string";
+
+document.write(A);
+
+document.write("This is a quote, \"here is the quote!\"<br>");
+
+document.write("Hello,"
++ "how are you");
+
+
+var R = "Roak" + "Panamera D"
+document.write(R);
+
+var family = "Roak", dog = "goobis", kid = "abyea";
+document.write(dog);
+
+document.write(3+3);
+
+function My_First_Function() {
+    var str = "This is the button text";
+    document.getElementById("Button_Text").innerHTML = str;
+}
