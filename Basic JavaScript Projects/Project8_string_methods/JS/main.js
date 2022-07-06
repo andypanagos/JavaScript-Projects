147509dd6c94de87f0ba5ebd504c3083442402b0
andypanagos <andypanagos2@gmail.com>
2022-07-05 23:24:41 -0400

    project8

---
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -0,0 +1,32 @@
+function slice_Method() {
+    var Sentence = "All work and no play makes Johnny a dull boy.";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section.toUpperCase();
+}
+function slice_Search() {
+    var A = "All work and no play makes Johnny a dull boy.";
+    document.getElementById("Slices").innerHTML = A.search("play")
+}
+
+function string_Method() {
+    var X = 182;
+    document.getElementById("Numbers_to_string").innerHTML = X.toString();
+}
+
+
+function precision_Method() {
+    var X = 12938.3021988765;
+    document.getElementById("Precision").innerHTML = X.toPrecision(10);
+}
+
+function fixed_Method() {
+    var A = 6.346457235;
+    var B = A.toFixed();
+    document.getElementById("Fixed").innerHTML = B
+}
+
+function value_Method() {
+let text = "Hello World"; 
+let result = text.valueOf() 
+document.getElementById("Value").innerHTML = result
+}
