c5118e9ae8fa17fbeb1132a20fa1974268f0cce8
andypanagos <andypanagos2@gmail.com>
2022-07-08 15:00:24 -0400

    updated concat method

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -30,3 +30,13 @@ let text = "Hello World";
 let result = text.valueOf() 
 document.getElementById("Value").innerHTML = result
 }
+
+
+function full_Sentence() {
+    var part_1 = "I have";
+    var part_2 = "made this";
+    var part_3 = "into a complete";
+    var part_4 = "sentence.";
+    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    document.getElementById("Concatenate").innerHTML = whole_sentence
+}
